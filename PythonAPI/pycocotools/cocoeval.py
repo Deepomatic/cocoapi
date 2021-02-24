c483a92230df170f6a5a6743fd9a75b68824a5fd
Antje Farnier <antje@deepomatic.com>
2021-02-24 16:21:47 +0100

    Modularise summary function

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -3,7 +3,7 @@ __author__ = 'tsungyi'
 import numpy as np
 import datetime
 import time
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 from . import mask as maskUtils
 import copy
 
@@ -123,14 +123,14 @@ class COCOeval:
         Run per image evaluation on given images and store results (a list of dict) in self.evalImgs
         :return: None
         '''
-        tic = time.time()
-        print('Running per image evaluation...')
+        # tic = time.time()
+        # print('Running per image evaluation...')
         p = self.params
         # add backward compatibility if useSegm is specified in params
         if not p.useSegm is None:
             p.iouType = 'segm' if p.useSegm == 1 else 'bbox'
             print('useSegm (deprecated) is not None. Running {} evaluation'.format(p.iouType))
-        print('Evaluate annotation type *{}*'.format(p.iouType))
+        # print('Evaluate annotation type *{}*'.format(p.iouType))
         p.imgIds = list(np.unique(p.imgIds))
         if p.useCats:
             p.catIds = list(np.unique(p.catIds))
@@ -157,8 +157,8 @@ class COCOeval:
                  for imgId in p.imgIds
              ]
         self._paramsEval = copy.deepcopy(self.params)
-        toc = time.time()
-        print('DONE (t={:0.2f}s).'.format(toc-tic))
+        # toc = time.time()
+        # print('DONE (t={:0.2f}s).'.format(toc - tic))
 
     def computeIoU(self, imgId, catId):
         p = self.params
@@ -318,8 +318,8 @@ class COCOeval:
         :param p: input params for evaluation
         :return: None
         '''
-        print('Accumulating evaluation results...')
-        tic = time.time()
+        # print('Accumulating evaluation results...')
+        # tic = time.time()
         if not self.evalImgs:
             print('Please run evaluate() first')
         # allows input customized parameters
@@ -416,81 +416,53 @@ class COCOeval:
             'recall':   recall,
             'scores': scores,
         }
-        toc = time.time()
-        print('DONE (t={:0.2f}s).'.format( toc-tic))
+        # toc = time.time()
+        # print('DONE (t={:0.2f}s).'.format(toc - tic))
+
+    def _summarize(self, ap=1, iouThr=None, areaRng='all', maxDets=100 ):
+        p = self.params
+        iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
+        titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
+        typeStr = '(AP)' if ap==1 else '(AR)'
+        iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+            if iouThr is None else '{:0.2f}'.format(iouThr)
+
+        aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
+        mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+        if ap == 1:
+            # dimension of precision: [TxRxKxAxM]
+            s = self.eval['precision']
+            # IoU
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+            s = s[:,:,:,aind,mind]
+        else:
+            # dimension of recall: [TxKxAxM]
+            s = self.eval['recall']
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+            s = s[:,:,aind,mind]
+        if len(s[s>-1])==0:
+            mean_s = -1
+        else:
+            mean_s = np.mean(s[s>-1])
+        # print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
+        return mean_s
 
     def summarize(self):
         '''
         Compute and display summary metrics for evaluation results.
         Note this functin can *only* be applied on the default parameter setting
         '''
-        def _summarize( ap=1, iouThr=None, areaRng='all', maxDets=100 ):
-            p = self.params
-            iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
-            titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
-            typeStr = '(AP)' if ap==1 else '(AR)'
-            iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
-                if iouThr is None else '{:0.2f}'.format(iouThr)
-
-            aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
-            mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
-            if ap == 1:
-                # dimension of precision: [TxRxKxAxM]
-                s = self.eval['precision']
-                # IoU
-                if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
-                    s = s[t]
-                s = s[:,:,:,aind,mind]
-            else:
-                # dimension of recall: [TxKxAxM]
-                s = self.eval['recall']
-                if iouThr is not None:
-                    t = np.where(iouThr == p.iouThrs)[0]
-                    s = s[t]
-                s = s[:,:,aind,mind]
-            if len(s[s>-1])==0:
-                mean_s = -1
-            else:
-                mean_s = np.mean(s[s>-1])
-            print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
-            return mean_s
-        def _summarizeDets():
-            stats = np.zeros((12,))
-            stats[0] = _summarize(1)
-            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
-            stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
-            stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
-            stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
-            return stats
-        def _summarizeKps():
-            stats = np.zeros((10,))
-            stats[0] = _summarize(1, maxDets=20)
-            stats[1] = _summarize(1, maxDets=20, iouThr=.5)
-            stats[2] = _summarize(1, maxDets=20, iouThr=.75)
-            stats[3] = _summarize(1, maxDets=20, areaRng='medium')
-            stats[4] = _summarize(1, maxDets=20, areaRng='large')
-            stats[5] = _summarize(0, maxDets=20)
-            stats[6] = _summarize(0, maxDets=20, iouThr=.5)
-            stats[7] = _summarize(0, maxDets=20, iouThr=.75)
-            stats[8] = _summarize(0, maxDets=20, areaRng='medium')
-            stats[9] = _summarize(0, maxDets=20, areaRng='large')
-            return stats
         if not self.eval:
             raise Exception('Please run accumulate() first')
-        iouType = self.params.iouType
-        if iouType == 'segm' or iouType == 'bbox':
-            summarize = _summarizeDets
-        elif iouType == 'keypoints':
-            summarize = _summarizeKps
-        self.stats = summarize()
+
+        stats = np.zeros((len(self.params.summary_config, )))
+        for i, value in enumerate(self.params.summary_config.values()):
+            stats[i] = self._summarize(**value)
+        self.stats = stats
 
     def __str__(self):
         self.summarize()
@@ -509,6 +481,20 @@ class Params:
         self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
         self.areaRngLbl = ['all', 'small', 'medium', 'large']
         self.useCats = 1
+        self.summary_config = OrderedDict({
+            'Precision/mAP': {'ap': 1},
+            'Precision/mAP@.50IOU': {"ap": 1, "iouThr": .5, "maxDets": self.maxDets[2]},
+            'Precision/mAP@.75IOU': {"ap": 1, "iouThr": .75, "maxDets": self.maxDets[2]},
+            'Precision/mAP (small)': {"ap": 1, "areaRng": 'small', "maxDets": self.maxDets[2]},
+            'Precision/mAP (medium)': {"ap": 1, "areaRng": 'medium', "maxDets": self.maxDets[2]},
+            'Precision/mAP (large)': {"ap": 1, "areaRng": 'large', "maxDets": self.maxDets[2]},
+            'Recall/AR@1': {"ap": 0, "maxDets": self.maxDets[0]},
+            'Recall/AR@10': {"ap": 0, "maxDets": self.maxDets[1]},
+            'Recall/AR@100': {"ap": 0, "maxDets": self.maxDets[2]},
+            'Recall/AR@100 (small)': {"ap": 0, "areaRng": 'small', "maxDets": self.maxDets[2]},
+            'Recall/AR@100 (medium)': {"ap": 0, "areaRng": 'medium', "maxDets": self.maxDets[2]},
+            'Recall/AR@100 (large)': {"ap": 0, "areaRng": 'large', "maxDets": self.maxDets[2]},
+        })
 
     def setKpParams(self):
         self.imgIds = []
@@ -521,6 +507,18 @@ class Params:
         self.areaRngLbl = ['all', 'medium', 'large']
         self.useCats = 1
         self.kpt_oks_sigmas = np.array([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62,.62, 1.07, 1.07, .87, .87, .89, .89])/10.0
+        self.summary_config = OrderedDict({
+            'Precision/mAP': {"ap": 1, "maxDets": 20},
+            'Precision/mAP@.50IOU': {"ap": 1, "maxDets": 20, "iouThr": .5},
+            'Precision/mAP@.75IOU': {"ap": 1, "maxDets": 20, "iouThr": .75},
+            'Precision/mAP (medium)': {"ap": 1, "maxDets": 20, "areaRng": 'medium'},
+            'Precision/mAP (large)': {"ap": 1, "maxDets": 20, "areaRng": 'large'},
+            'Recall/AR': {"ap": 0, "maxDets": 20},
+            'Recall/AR@.50IOU': {"ap": 0, "maxDets": 20, "iouThr": .5},
+            'Recall/AR@.75IOU': {"ap": 0, "maxDets": 20, "iouThr": .75},
+            'Recall/AR (medium)': {"ap": 0, "maxDets": 20, "areaRng": 'medium'},
+            'Recall/AR (large)': {"ap": 0, "maxDets": 20, "areaRng": 'large'},
+        })
 
     def __init__(self, iouType='segm'):
         if iouType == 'segm' or iouType == 'bbox':
